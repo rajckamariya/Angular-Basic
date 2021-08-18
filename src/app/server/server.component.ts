5b1f5c9fb891971059fa03144017bdf84d057eef
rajckamariya <r.c.kamariya@gmail.com>
2021-08-18 12:22:09 +0530

    ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'Offline';
-
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+  }
   getServerStatus() {
     return this.serverStatus;
+  }
+  getColor() {
+    return this.serverStatus === 'Online' ? 'green' : 'red';
   }
 }
